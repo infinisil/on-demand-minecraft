0f5bab879aeff73c6a3055057776b8daa4843b88
Silvan Mosberger <contact@infinisil.com>
2020-04-07 21:43:26 +0200

    Messy working implementation of handshake and status messages

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,121 @@
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Socket
 import Network.Socket
+import Network.Socket.ByteString
 import System.IO
 
+import System.IO.ByteBuffer
+import Minecraft
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Store
+import Data.Text.Encoding
+
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
+import Control.Concurrent (forkIO, threadDelay)
+
+import qualified Data.Aeson as A
+import qualified Data.ByteString.Lazy as LBS
+
+consumeExactly :: ByteBuffer -> TVar Int -> TVar Bool -> Int -> IO (Maybe ByteString)
+consumeExactly buffer var term count = do
+  putStrLn $ "Wanting to consume exactly " <> show count <> " bytes, waiting for that"
+  res <- atomically $ do
+    termVal <- readTVar term
+    if termVal then
+      return False
+    else do
+      available <- readTVar var
+      check (available >= count)
+      modifyTVar var (subtract count)
+      return True
+  if res then do
+    putStrLn $ "Got " <> show count <> " bytes, getting and returning them"
+    Right bytes <- consume buffer count
+    return (Just bytes)
+  else return Nothing
+
+readPacket :: ByteBuffer -> TVar Int -> TVar Bool -> IO (Maybe ByteString)
+readPacket buffer var term = do
+  putStrLn "Trying to read the size"
+  getSize mempty >>= \case
+    Nothing -> return Nothing
+    Just (VarInt size) -> do
+      putStrLn $ "Size is " <> show size <> ", trying to get that many bytes"
+      consumeExactly buffer var term (fromIntegral size)
+  where
+    getSize :: ByteString -> IO (Maybe VarInt)
+    getSize previous = do
+      putStrLn "For getting the size, reading the next byte"
+      consumeExactly buffer var term 1 >>= \case
+        Nothing -> return Nothing
+        Just onebyte -> do
+          let bytes = previous <> onebyte
+          putStrLn "For getting the size, try to decode with the next byte appended"
+          case decode bytes of
+            Left exception -> do
+              putStrLn $ "For getting the size, not yet enough bytes, probably: " <> show exception
+              getSize bytes
+            Right count -> return (Just count)
+
+statusPacket :: ServerStatusPacket
+statusPacket = ServerStatusResponse (MCString encoded) where
+  response = Response
+      { response_version = ResponseVersion "1.15.2" 578
+      , response_players = ResponsePlayers 100000000 100000001 []
+      , response_description = ResponseDescription "NANI SORE"
+      }
+  encoded = decodeUtf8 (LBS.toStrict (A.encode response))
+
 clientHandler :: Socket -> IO ()
-clientHandler socket = do
-  handle <- socketToHandle socket ReadWriteMode
-  hSetBuffering handle LineBuffering
-  let loop = do
-        isEOF <- hIsEOF handle
-        if isEOF then hClose handle
-        else do
-          line <- hGetLine handle
-          if line == "quit" then do
-            hPutStrLn handle "See ya"
-            hClose handle
-          else do
-            hPutStrLn handle line
-            loop
-  loop
+clientHandler socket = with Nothing $ \buffer -> do
+  var <- newTVarIO 0
+  term <- newTVarIO False
+  forkIO $ receiveLoop socket buffer var term
+  let loop state = readPacket buffer var term >>= \case
+        Nothing -> putStrLn "Terminated"
+        Just packet -> case state of
+          (VarInt 0) -> case decode packet of
+            Left exc -> print exc
+            Right (HandshakePacket handshake) -> do
+              putStrLn $ "Received handshake: " <> show handshake
+              loop (nextState handshake)
+          (VarInt 1) -> case decode packet of
+            Left exc -> print exc
+            Right ClientStatusRequest -> do
+              putStrLn "Received request!"
+              let bytes = encode statusPacket
+                  allBytes = encode (VarInt (fromIntegral (BS.length bytes))) <> bytes
+              sendAll socket allBytes
+              loop state
+            Right (ClientStatusPing val) -> do
+              putStrLn $ "Received ping: " <> show val
+              let bytes = encode (ServerStatusPong val)
+                  allBytes = encode (VarInt (fromIntegral (BS.length bytes))) <> bytes
+              sendAll socket allBytes
+              loop state
+          _ -> putStrLn $ "State " <> show state <> " not supported yet"
+  loop (VarInt 0)
+  gracefulClose socket 5000
+
+receiveLoop :: Socket -> ByteBuffer -> TVar Int -> TVar Bool -> IO ()
+receiveLoop socket buffer var term = do
+  putStrLn "Trying to receive up to 4096 bytes"
+  bytes <- recv socket 4096
+  if BS.null bytes then do
+    atomically $ writeTVar term True
+    putStrLn "Seems like no more bytes could be received"
+  else do
+    putStrLn $ "Was able to receive " <> show (BS.length bytes) <> " bytes, copying to buffer"
+    copyByteString buffer bytes
+    atomically $ modifyTVar' var (+ BS.length bytes)
+    receiveLoop socket buffer var term
+
 
 main :: IO ()
 main = do
--- lib/Minecraft.hs
+++ lib/Minecraft.hs
@@ -1,19 +1,37 @@
+{-# LANGUAGE BinaryLiterals      #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE BinaryLiterals #-}
+{-# LANGUAGE TupleSections       #-}
 
-module Minecraft (VarInt(..), MCString(..))where
+module Minecraft where
 
 import Data.Store
-import Data.Int (Int32)
+import Data.Store.Internal (combineSize, combineSizeWith, getSizeWith)
+import Data.Int (Int32, Int64)
 import Data.Bits
-import Data.Word (Word8, Word32)
+import Data.Word (Word8, Word32, Word16)
 import Data.Maybe (fromJust)
 import qualified Data.ByteString as BS
 
-import Data.Text
+import Data.Text (Text)
+import qualified Data.Text as T
+import GHC.Generics
+import Data.List (groupBy)
 import Data.Text.Encoding
 import Data.Foldable (traverse_)
 import Control.Monad (replicateM)
+
+import Control.Applicative
+import Data.Void (absurd)
+import Data.Functor.Contravariant.Divisible
+import qualified Data.Aeson as A
+import qualified Data.ByteString.Lazy as LBS
 
 -- See https://wiki.vg/Protocol#VarInt_and_VarLong
 newtype VarInt = VarInt Int32 deriving (Show, Eq)
@@ -50,3 +68,129 @@ instance Store MCString where
     VarInt len <- peek :: Peek VarInt
     bytes :: [Word8] <- replicateM (fromIntegral len) peek
     pure $ MCString (decodeUtf8 (BS.pack bytes))
+
+type ProtocolVersion = VarInt
+type ServerAddress = MCString
+type ServerPort = Word16
+type ConnectionState = VarInt
+
+data Handshake = Handshake
+  { protocolVersion :: ProtocolVersion
+  , serverAddress :: ServerAddress
+  , serverPort :: ServerPort
+  , nextState :: ConnectionState
+  } deriving (Generic, Show, Eq)
+
+instance Store Handshake
+testHandshake = Handshake (VarInt 10) (MCString "hello") 10 (VarInt 10)
+
+newtype HandshakePacket = HandshakePacket { handshakePacketData :: Handshake }
+  deriving (Show, Eq)
+
+type PacketID = VarInt
+handshakePacketId :: HandshakePacket -> PacketID
+handshakePacketId (HandshakePacket _) = VarInt 0
+
+instance Store HandshakePacket where
+  size = combineSize handshakePacketId handshakePacketData
+  poke value = poke (handshakePacketId value) >> poke (handshakePacketData value)
+  peek = do
+    VarInt readPacketId :: PacketID <- peek
+    case readPacketId of
+      0 -> HandshakePacket <$> peek
+      _ -> fail $ "Packet ID " <> show readPacketId <> " not supported"
+
+data ClientStatusPacket = ClientStatusRequest | ClientStatusPing Int64
+
+instance Store ClientStatusPacket where
+  size = choose f (combineSize (const (VarInt 0)) id) (combineSize (const (VarInt 1)) id) where
+    f :: ClientStatusPacket -> Either () Int64
+    f ClientStatusRequest = Left ()
+    f (ClientStatusPing x) = Right x
+  poke ClientStatusRequest = poke (VarInt 0)
+  poke (ClientStatusPing val) = poke (VarInt 1) >> poke val
+  peek = peek >>= \case
+    VarInt 0 -> pure ClientStatusRequest
+    VarInt 1 -> ClientStatusPing <$> peek
+
+
+data ServerStatusPacket = ServerStatusResponse MCString | ServerStatusPong Int64 deriving Show
+
+instance Store ServerStatusPacket where
+  size = choose f (combineSize (const (VarInt 0)) id) (combineSize (const (VarInt 1)) id) where
+    f :: ServerStatusPacket -> Either MCString Int64
+    f (ServerStatusResponse val) = Left val
+    f (ServerStatusPong x) = Right x
+  poke (ServerStatusResponse val) = poke (VarInt 0) >> poke val
+  poke (ServerStatusPong val) = poke (VarInt 1) >> poke val
+  peek = peek >>= \case
+    VarInt 0 -> ServerStatusResponse <$> peek
+    VarInt 1 -> ServerStatusPong <$> peek
+
+instance Divisible Size where
+  divide f = combineSizeWith (fst . f) (snd . f)
+  conquer = ConstSize 0
+
+instance Decidable Size where
+  choose f s t = VarSize (either (getSizeWith s) (getSizeWith t) . f)
+  lose f = VarSize (absurd . f)
+
+instance Num (Size a) where
+  VarSize l + VarSize r = VarSize (liftA2 (+) l r)
+  VarSize l + ConstSize r = VarSize ((+ r) <$> l)
+  ConstSize l + VarSize r = VarSize ((+ l) <$> r)
+  ConstSize l + ConstSize r = ConstSize (l + r)
+
+  VarSize l * VarSize r = VarSize (liftA2 (*) l r)
+  VarSize l * ConstSize r = VarSize ((* r) <$> l)
+  ConstSize l * VarSize r = VarSize ((* l) <$> r)
+  ConstSize l * ConstSize r = ConstSize (l * r)
+
+  abs (ConstSize v) = ConstSize (abs v)
+  abs (VarSize f) = VarSize (abs <$> f)
+
+  signum (ConstSize v) = ConstSize (signum v)
+  signum (VarSize f) = VarSize (signum <$> f)
+
+  fromInteger int = ConstSize (fromInteger int)
+
+  negate (ConstSize v) = ConstSize (negate v)
+  negate (VarSize f) = VarSize (negate <$> f)
+
+jsonOptions :: A.Options
+jsonOptions = A.defaultOptions
+  { A.fieldLabelModifier = last . groupBy (\a b -> a /= '_' && b /= '_')
+  }
+
+data ResponseVersion = ResponseVersion
+  { response_version_name :: Text
+  , response_version_protocol :: Int
+  } deriving (Generic, Show)
+
+instance A.ToJSON ResponseVersion where
+  toEncoding = A.genericToEncoding jsonOptions
+
+data ResponsePlayers = ResponsePlayers
+  { response_players_max :: Int
+  , response_players_online :: Int
+  , response_players_sample :: [()]
+  } deriving (Generic, Show)
+
+instance A.ToJSON ResponsePlayers where
+  toEncoding = A.genericToEncoding jsonOptions
+
+newtype ResponseDescription = ResponseDescription
+  { response_description_text :: Text
+  } deriving (Generic, Show)
+
+instance A.ToJSON ResponseDescription where
+  toEncoding = A.genericToEncoding jsonOptions
+
+data Response = Response
+  { response_version :: ResponseVersion
+  , response_players :: ResponsePlayers
+  , response_description :: ResponseDescription
+  } deriving (Generic, Show)
+
+instance A.ToJSON Response where
+  toEncoding = A.genericToEncoding jsonOptions
