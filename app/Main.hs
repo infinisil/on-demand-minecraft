b97f64e3d66efe3f26097b592aabddf75e3fb4fa
Silvan Mosberger <contact@infinisil.com>
2020-04-09 01:24:13 +0200

    Implement TCP connection relaying

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,9 @@ module Main where
 import Minecraft
 
 import Network.Socket
+import Network.Socket.ByteString
+import qualified Data.ByteString as BS
+import Control.Exception
 import System.IO
 import Control.Concurrent (forkIO)
 import Control.Monad
@@ -21,6 +24,9 @@ import Polysemy
 import Polysemy.Async
 import Polysemy.Trace
 import Polysemy.Error
+import Control.Concurrent.STM
+
+import qualified Control.Concurrent.Async as A
 
 
 shallowServer :: forall r . Members '[Async, Final IO, Trace, Minecraft Server] r => Sem r ()
@@ -72,6 +78,29 @@ runServer = do
     trace $ "Accepted connection from " <> show addr
     async $ runError $ runMinecraft socket shallowServer
 
+relayServer :: Members '[Trace, Async, Final IO] r => Sem r ()
+relayServer = do
+  serverSocket <- embedFinal $ socket AF_INET Stream defaultProtocol
+  let hints = defaultHints
+        { addrFlags = [AI_PASSIVE]
+        , addrSocketType = Stream
+        }
+  -- TODO: Listen on all addresses to support both IPv4 and IPv6?
+  addr <- embedFinal $ head <$> getAddrInfo (Just hints) Nothing (Just "25565")
+  embedFinal $ bind serverSocket $ addrAddress addr
+  embedFinal $ listen serverSocket 16
+  forever $ do
+    (serverSocketAccepted, addr) <- embedFinal $ accept serverSocket
+    trace $ "Accepted connection from " <> show addr
+
+    let hints = defaultHints { addrSocketType = Stream }
+    addr <- embedFinal $ head <$> getAddrInfo (Just hints) (Just "infinisil.com") (Just "25565")
+    trace $ "Address is " <> show addr
+    clientSocket <- embedFinal $ socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+    embedFinal $ connect clientSocket $ addrAddress addr
+
+    embedFinal $ tcpRelay serverSocketAccepted clientSocket
+
 queryServerStatus :: Members '[Trace, Async, Final IO, Error String] r => HostName -> Sem r Response
 queryServerStatus hostname = do
   let hints = defaultHints { addrSocketType = Stream }
@@ -92,6 +121,20 @@ statusQueryClient = do
   sendPacket ClientPacketRequest
   receivePacket @StatusState >>= \case
     ServerPacketResponse response -> return response
+
+tcpRelay :: Socket -> Socket -> IO ()
+tcpRelay a b = do
+  void $ A.race (relay a b) (relay b a)
+  close a
+  close b
+  where
+  relay :: Socket -> Socket -> IO ()
+  relay from to = loop where
+    loop = do
+      bytes <- recv from 4096
+      unless (BS.null bytes) $ do
+        sendAll to bytes
+        loop
 
 main :: IO ()
 main = do
