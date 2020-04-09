4f654e2f388916499017f67b3139025e8962658c
Silvan Mosberger <contact@infinisil.com>
2020-04-10 00:01:11 +0200

    Fix Nix build, remove old test and minimize closure

--- default.nix
+++ default.nix
@@ -13,10 +13,9 @@ let
   hpkgs = pkgs.haskell.packages.ghc882.override (old: {
     overrides = lib.composeExtensions (old.overrides or (self: super: {})) (self: super: {
       on-demand-minecraft = self.callCabal2nix "on-demand-minecraft" (lib.sourceByRegex ./. [
-        "^app$"
-        "^lib$"
-        "^tests$"
-        "^.*\\.hs$"
+        "^app.*"
+        "^lib.*"
+        "^tests.*"
         "^.*\\.cabal$"
       ]) {};
 
@@ -39,65 +38,6 @@ let
 
   pkg = hpkgs.on-demand-minecraft;
 
-in pkg // {
+in hlib.justStaticExecutables pkg // {
   inherit env hpkgs;
-
-  test =
-    let
-      testingLib = import (pkgs.path + "/nixos/lib/testing-python.nix") {
-        system = builtins.currentSystem;
-      };
-    in
-      testingLib.makeTest {
-        machine = {
-          systemd.sockets.on-demand-minecraft = {
-            wantedBy = [ "sockets.target" ];
-            socketConfig.ListenStream = "25565";
-          };
-
-          systemd.services.on-demand-minecraft = {
-            serviceConfig.ExecStart = "${pkg}/bin/on-demand-minecraft";
-          };
-        };
-        testScript = ''
-          machine.start()
-          machine.wait_for_unit("on-demand-minecraft.socket")
-
-          machine.require_unit_state("on-demand-minecraft.service", "inactive")
-
-          res = machine.succeed('echo "Hello\nquit" | nc localhost 25565')
-          if res != "Hello\nSee ya\n":
-              raise Exception('Did not get the expected output, but "{}" instead'.format(res))
-
-          machine.sleep(0.1)
-          machine.require_unit_state("on-demand-minecraft.service", "inactive")
-
-          res = machine.succeed('echo "Hi there\nquit" | nc localhost 25565')
-          if res != "Hi there\nSee ya\n":
-              raise Exception('Did not get the expected output, but "{}" instead'.format(res))
-
-          machine.sleep(0.1)
-          machine.require_unit_state("on-demand-minecraft.service", "inactive")
-
-          # Check that the service can accept multiple connections at a time
-          res = machine.succeed(
-              """
-                  {
-                    echo "level1"
-                    sleep 1
-                    echo "level2\nquit" | nc localhost 25565
-                    sleep 1
-                    echo "quit"
-                  } | nc localhost 25565
-              """
-          )
-          if res != "level1\nlevel2\nSee ya\nSee ya\n":
-              raise Exception('Did not get the expected output, but "{}" instead'.format(res))
-
-          machine.sleep(0.1)
-          machine.require_unit_state("on-demand-minecraft.service", "inactive")
-        '';
-
-      };
-
 }
