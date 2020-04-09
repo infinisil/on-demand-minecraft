b406b955aad51ffb0ffd64046e3d72bca3e93ce3
Silvan Mosberger <contact@infinisil.com>
2020-04-09 05:23:21 +0200

    Initial skeleton for final product
    The main flow is there

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE GADTs               #-}
@@ -16,21 +17,45 @@ import Minecraft
 import Network.Socket
 import Network.Socket.ByteString
 import qualified Data.ByteString as BS
-import Control.Exception
 import System.IO
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad
 import Polysemy
 import Polysemy.Async
 import Polysemy.Trace
 import Polysemy.Error
+import Polysemy.Resource
+import Polysemy.AtomicState
+import Polysemy.Reader
+import           Polysemy.Final
+
 import Control.Concurrent.STM
+import Control.Monad (join)
+import qualified System.Timeout
+import Data.Text (Text)
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import System.Directory
 
 import qualified Control.Concurrent.Async as A
+import qualified Control.Exception
 
 
-shallowServer :: forall r . Members '[Async, Final IO, Trace, Minecraft Server] r => Sem r ()
-shallowServer = receivePacket @HandshakingState >>= \case
+data Timeout m a where
+  Timeout :: Int -> m a -> Timeout m (Maybe a)
+
+makeSem ''Timeout
+
+runTimeout :: Member (Final IO) r => Sem (Timeout ': r) a -> Sem r a
+runTimeout = interpretFinal $ \case
+  Timeout micros action -> do
+    ins <- getInspectorS
+    m' <- runS action
+    liftS $ join <$> System.Timeout.timeout micros (inspect ins <$> m')
+
+shallowServer :: forall r . Members '[Reader Config, AtomicState UpstreamState, Async, Final IO, Trace, Minecraft Server] r => UpstreamState -> Sem r ()
+shallowServer currentState = receivePacket @HandshakingState >>= \case
   ClientPacketHandshake handshake -> case nextState handshake of
     StatusState -> clientLoopStatus
     LoginState -> clientLoopLogin
@@ -40,18 +65,14 @@ shallowServer = receivePacket @HandshakingState >>= \case
     clientLoopStatus = maybeReceivePacket @StatusState >>= \case
       Nothing -> trace "Client disconnected"
       Just ClientPacketRequest -> do
-        result <- runError $ queryServerStatus "infinisil.com"
-        case result of
-          Left err -> do
-            sendPacket $ ServerPacketResponse $ Response
-              { response_version = ResponseVersion "1.15.2" 578
-              , response_players = ResponsePlayers (-1) 0
-              , response_description = Chat "Error"
-              }
-            clientLoopStatus
-          Right response -> do
-            sendPacket $ ServerPacketResponse response
-            clientLoopStatus
+        sendPacket $ ServerPacketResponse $ Response
+          { response_version = ResponseVersion "1.15.2" 578
+          , response_players = ResponsePlayers (-1) 0
+          , response_description = Chat $ case currentState of
+              Down -> "Server is down"
+              Starting -> "Server is starting"
+          }
+        clientLoopStatus
       Just (ClientPacketPing nonce) -> do
         sendPacket $ ServerPacketPong nonce
         clientLoopStatus
@@ -59,71 +80,28 @@ shallowServer = receivePacket @HandshakingState >>= \case
     clientLoopLogin :: Sem r ()
     clientLoopLogin = receivePacket @LoginState >>= \case
       ClientPacketLoginStart name -> do
-        sendPacket $ ServerPacketLoginSuccess name "01e2780a-1334-4891-95dd-506e58dcebb9"
-        sendPacket $ ServerPacketDisconnect "starting"
+        muuid <- Map.lookup name . config_whitelist <$> ask
+        case muuid of
+          -- TODO: Figure out if actual servers respond like this
+          Nothing -> sendPacket $ ServerPacketDisconnect "You're not whitelisted"
+          Just uuid -> do
+            startUpstream
+            sendPacket $ ServerPacketLoginSuccess name uuid
+            sendPacket $ ServerPacketDisconnect $ case currentState of
+              Down -> "You're whitelisted, now starting server, please wait"
+              Starting -> "Server starting, please wait"
 
-runServer :: Members '[Trace, Async, Final IO] r => Sem r ()
-runServer = do
-  serverSocket <- embedFinal $ socket AF_INET Stream defaultProtocol
-  let hints = defaultHints
-        { addrFlags = [AI_PASSIVE]
-        , addrSocketType = Stream
-        }
-  -- TODO: Listen on all addresses to support both IPv4 and IPv6?
-  addr <- embedFinal $ head <$> getAddrInfo (Just hints) Nothing (Just "25565")
-  embedFinal $ bind serverSocket $ addrAddress addr
-  embedFinal $ listen serverSocket 16
-  forever $ do
-    (socket, addr) <- embedFinal $ accept serverSocket
-    trace $ "Accepted connection from " <> show addr
-    async $ runError $ runMinecraft socket shallowServer
+startUpstream :: Members '[Trace, AtomicState UpstreamState] r => Sem r ()
+startUpstream = do
+  -- TODO: Check if already starting, if not, make it so
+  needsStarting <- atomicState $ \case
+    Down -> (Starting, True)
+    other -> (other, False)
+  when needsStarting $ do
+    trace "Would start upstream now"
 
-relayServer :: Members '[Trace, Async, Final IO] r => Sem r ()
-relayServer = do
-  serverSocket <- embedFinal $ socket AF_INET Stream defaultProtocol
-  let hints = defaultHints
-        { addrFlags = [AI_PASSIVE]
-        , addrSocketType = Stream
-        }
-  -- TODO: Listen on all addresses to support both IPv4 and IPv6?
-  addr <- embedFinal $ head <$> getAddrInfo (Just hints) Nothing (Just "25565")
-  embedFinal $ bind serverSocket $ addrAddress addr
-  embedFinal $ listen serverSocket 16
-  forever $ do
-    (serverSocketAccepted, addr) <- embedFinal $ accept serverSocket
-    trace $ "Accepted connection from " <> show addr
-
-    let hints = defaultHints { addrSocketType = Stream }
-    addr <- embedFinal $ head <$> getAddrInfo (Just hints) (Just "infinisil.com") (Just "25565")
-    trace $ "Address is " <> show addr
-    clientSocket <- embedFinal $ socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-    embedFinal $ connect clientSocket $ addrAddress addr
-
-    embedFinal $ tcpRelay serverSocketAccepted clientSocket
-
-queryServerStatus :: Members '[Trace, Async, Final IO, Error String] r => HostName -> Sem r Response
-queryServerStatus hostname = do
-  let hints = defaultHints { addrSocketType = Stream }
-  addr <- embedFinal $ head <$> getAddrInfo (Just hints) (Just hostname) (Just "25565")
-  trace $ "Address is " <> show addr
-  clientSocket <- embedFinal $ socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-  embedFinal $ connect clientSocket $ addrAddress addr
-  runMinecraft clientSocket statusQueryClient
-
-statusQueryClient :: Member (Minecraft Client) r => Sem r Response
-statusQueryClient = do
-  sendPacket $ ClientPacketHandshake $ Handshake
-    { protocolVersion = 578
-    , serverAddress = ""
-    , serverPort = 0
-    , nextState = StatusState
-    }
-  sendPacket ClientPacketRequest
-  receivePacket @StatusState >>= \case
-    ServerPacketResponse response -> return response
-
-tcpRelay :: Socket -> Socket -> IO ()
-tcpRelay a b = do
+tcpRelay :: Member (Embed IO) r => Socket -> Socket -> Sem r ()
+tcpRelay a b = embed $ do
   void $ A.race (relay a b) (relay b a)
   close a
   close b
@@ -136,7 +114,136 @@ tcpRelay a b = do
         sendAll to bytes
         loop
 
+data UpstreamState = Down | Starting | Up deriving (Show, Read)
+
+type Whitelist = Map Text Text
+
+data Config = Config
+  { config_whitelist :: Whitelist
+  , config_upstream :: AddrInfo
+  }
+
+whitelist :: Whitelist
+whitelist = Map.fromList
+  [ ("infinisil", "01e2780a-1334-4891-95dd-506e58dcebb9")
+  ]
+
+getUpstreamAddr :: Member (Embed IO) r => Sem r AddrInfo
+getUpstreamAddr = do
+  let hints = defaultHints { addrSocketType = Stream }
+  embed $ head <$> getAddrInfo (Just hints) (Just "127.0.0.1") (Just "25566")
+
+
+initListenSocket :: Member (Embed IO) r => Sem r Socket
+initListenSocket = embed $ do
+  let hints = defaultHints
+        { addrFlags = [AI_PASSIVE]
+        , addrSocketType = Stream
+        }
+  -- TODO: Listen on all addresses to support both IPv4 and IPv6?
+  addr <- addrAddress . head <$> getAddrInfo (Just hints) Nothing (Just "25565")
+
+  serverSocket <- socket AF_INET Stream defaultProtocol
+  bind serverSocket addr
+  listen serverSocket 16
+  return serverSocket
+
+statePath :: FilePath
+statePath = "upstreamstate"
+
+restoreState :: Member (Embed IO) r => Sem r (TVar UpstreamState)
+restoreState = do
+  exists <- embed $ doesFileExist statePath
+  if exists then do
+    value <- embed $ read <$> readFile statePath
+    embed $ newTVarIO value
+  else embed $ newTVarIO Down
+
+saveState :: Member (Embed IO) r => TVar UpstreamState -> Sem r ()
+saveState state = do
+  value <- embed $ readTVarIO state
+  embed $ writeFile statePath $ show value
+
 main :: IO ()
-main = do
-  hSetBuffering stdout LineBuffering
-  runFinal $ asyncToIOFinal $ embedToFinal $ traceToIO runServer
+main = runFinal $ asyncToIOFinal $ resourceToIOFinal $ embedToFinal $ traceToIO $ do
+  embed $ hSetBuffering stdout LineBuffering
+  upstream <- getUpstreamAddr
+  bracket restoreState saveState
+    $ \state -> runReader (Config whitelist upstream)
+    $ runAtomicStateTVar state runServer
+
+runServer :: Members '[Trace, Async, Embed IO, Final IO, AtomicState UpstreamState, Reader Config] r => Sem r ()
+runServer = do
+  serverSocket <- initListenSocket
+  forever $ do
+    peer <- embedFinal $ accept serverSocket
+    async $ do
+      result <- runError $ handlePeer peer
+      case result of
+        Left err -> trace $ "Errur during peer handling :" <> err
+        Right result -> trace "Finished handling peer successfully"
+
+updateAndGetState :: Members '[Async, Reader Config, Final IO, Embed IO, Error String, Trace, AtomicState UpstreamState] r => Sem r UpstreamState
+updateAndGetState = do
+  currentState <- atomicGet
+  up <- isUp
+  let newState = case (up, currentState) of
+        (True, _) -> Up
+        (False, Starting) -> Starting
+        (False, _) -> Down
+  atomicPut newState
+  return newState
+
+
+isUp :: Members '[Async, Trace, Final IO, Embed IO, Error String, Reader Config] r => Sem r Bool
+isUp = do
+  trace "Trying to determine whether upstream is up"
+  result <- runTimeout $ timeout 1000000 $ do
+    upstreamSocket <- connectUpstream
+    runMinecraft upstreamSocket mcClient
+  case result of
+    Nothing -> do
+      trace "Timed or errored out, upstream is down"
+      return False
+    Just res -> do
+      trace $ "Didn't time out, upstream is up: " <> show res
+      return res
+  where
+    mcClient :: Members '[Trace, Minecraft Client] r => Sem r Bool
+    mcClient = do
+      sendPacket $ ClientPacketHandshake $ Handshake
+        { protocolVersion = 578
+        , serverAddress = ""
+        , serverPort = 25566
+        , nextState = StatusState
+        }
+      sendPacket ClientPacketRequest
+      receivePacket @StatusState >>= \case
+        ServerPacketResponse response -> do
+          trace $ show response
+          return True
+
+handlePeer :: Members '[Error String, Async, Trace, Embed IO, Final IO, AtomicState UpstreamState, Reader Config] r => (Socket, SockAddr) -> Sem r ()
+handlePeer (peerSocket, peerAddr) = do
+  trace $ "Accepted connection from " <> show peerAddr
+  state <- updateAndGetState
+  case state of
+    Up -> do
+      trace "Upstream is up, relaying connection"
+      runRelay peerSocket
+    _ -> do
+      trace "Upstream is not up, handling the connection locally"
+      runMinecraft peerSocket (shallowServer state)
+
+connectUpstream :: Members '[Error String, Embed IO, Reader Config] r => Sem r Socket
+connectUpstream = do
+  addr <- asks config_upstream
+  upstreamSocket <- embed $ socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  fromExceptionVia @Control.Exception.SomeException show $ connect upstreamSocket $ addrAddress addr
+  return upstreamSocket
+
+
+runRelay :: Members '[Error String, Embed IO, Reader Config] r => Socket -> Sem r ()
+runRelay peerSocket = do
+  upstreamSocket <- connectUpstream
+  tcpRelay peerSocket upstreamSocket
