144c4bdf0446c58d12494dc08c6d93b2074fc244
Silvan Mosberger <contact@infinisil.com>
2020-12-19 20:51:22 +0100

    1.16.3 -> 1.16.4

--- lib/Minecraft/Protocol.hs
+++ lib/Minecraft/Protocol.hs
@@ -17,7 +17,7 @@ import Data.Text (Text, unpack)
 queryStatus :: Members '[Trace, Minecraft Client] r => Sem r Response
 queryStatus = do
   sendPacket $ ClientPacketHandshake $ Handshake
-    { protocolVersion = 753
+    { protocolVersion = 754
     , serverAddress = ""
     , serverPort = 25565
     , nextState = StatusState
@@ -42,7 +42,7 @@ shallowServer whitelistCheck statusResponse loginResponse = receivePacket @Hands
       Nothing -> trace "Client disconnected"
       Just ClientPacketRequest -> do
         sendPacket $ ServerPacketResponse $ Response
-          { response_version = ResponseVersion "1.16.3" 753
+          { response_version = ResponseVersion "1.16.4" 754
           , response_players = ResponsePlayers (-1) 0
           , response_description = Chat statusResponse
           }
